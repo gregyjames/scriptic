e607eec79b8f2d91a910347eaab2ea457934f850
Greg James <gregvjames99@gmail.com>
2020-07-18 23:50:48 -0500

    Added Networking

---
+++ scriptic/Networking/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scriptic.Networking
+{
+    public class Methods
+    {
+        public static async Task StreamToFile(string url, string outFile)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
+                {
+                    using (Stream streamToWriteTo = File.Open(outFile, FileMode.Create))
+                    {
+                        await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                    }
+                }
+            }
+        }
+    }
+}
