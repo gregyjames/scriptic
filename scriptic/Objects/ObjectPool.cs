63c25e07a60795957a9dda4771256407b38cb643
Greg James <gregvjames99@gmail.com>
2020-07-18 21:44:46 -0500

    Added Project
    Initial Commit

---
+++ scriptic/Objects/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace scriptic.Objects
+{
+    public class ObjectPool<T>
+    {
+        private readonly ConcurrentBag<T> _poolInternal;
+        private readonly Func<T> generatorFunc;
+
+        public ObjectPool (int Count, Func<T> generatorFunction)
+        {
+            generatorFunc = generatorFunction;
+            _poolInternal = new ConcurrentBag<T>();
+            for (int i = 0; i < Count; i++)
+            {
+                _poolInternal.Add(generatorFunction.Invoke());
+            }
+        }
+
+        public T Get()
+        {
+            return _poolInternal.TryTake(out T item) ? item : generatorFunc();
+        }
+
+        public void Return(T item)
+        {
+            _poolInternal.Add(item);
+        }
+    }
+}
